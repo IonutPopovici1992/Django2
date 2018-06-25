b73e96e62843951bbb701ed45c72bce14092f37a
John <ionut_popovici92@yahoo.com>
2018-06-26 01:09:55 +0300

    Templates

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello, this is the home page...')
+    return render(request, 'home.html', {'message': 'This is the home page...'})
 
 def others(request):
     return HttpResponse('Hello, this is the others page...')
